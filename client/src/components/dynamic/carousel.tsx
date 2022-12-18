338a1282b7025e3de0a66bc17ff9d8af83045245
JosseMontano <josemariazambranamontano123@gmail.com>
2022-12-17 22:30:58 -0400

    refactoring carousel

--- client/src/components/dynamic/carousel.tsx
+++ client/src/components/dynamic/carousel.tsx
@@ -1,112 +1,97 @@
 import { RefObject, useRef } from "react";
 import styled from "styled-components";
+
 const Container = styled.div`
-  .slider-wrapper {
-    margin: 1rem;
-    position: relative;
-    /*     overflow: hidden; */
-  }
-
-  .slides-container {
-    display: flex;
-    /*  overflow: scroll;  */
-    overflow: hidden;
-    scroll-behavior: smooth;
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  .slide-arrow {
-    position: absolute;
-    display: flex;
-    top: 10px;
-    bottom: 0px;
-    margin: auto;
-    height: 4rem;
-    background-color: white;
-    border: none;
-    width: 2rem;
-    font-size: 3rem;
-    padding: 0;
-    cursor: pointer;
-    opacity: 0.5;
-    transition: opacity 100ms;
-    z-index: 999;
-  }
-
-  .slide-arrow:hover,
-  .slide-arrow:focus {
-    opacity: 1;
-  }
-
-  #slide-arrow-prev {
-    left: 0px;
-    border-radius: 2rem;
-    top: 45px;
-  }
-
-  #slide-arrow-next {
-    right: 20px;
-    border-radius: 2rem;
-    top: 45px;
-  }
-
+  margin: 1rem;
+  position: relative;
   .slide {
     width: 100%;
     height: 100%;
     flex: 1 0 100%;
   }
+  #prev {
+    left: 0px;
+    border-radius: 2rem;
+    top: 45px;
+  }
+  #next {
+    right: 20px;
+    border-radius: 2rem;
+    top: 45px;
+  }
 `;
+const ContainerSlide = styled.div`
+  display: flex;
+  /*  overflow: scroll;  */
+  overflow: hidden;
+  scroll-behavior: smooth;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+const Btn = styled.button`
+  position: absolute;
+  display: flex;
+  top: 10px;
+  bottom: 0px;
+  margin: auto;
+  height: 4rem;
+  background-color: white;
+  border: none;
+  width: 2rem;
+  font-size: 3rem;
+  padding: 0;
+  cursor: pointer;
+  opacity: 0.5;
+  transition: opacity 100ms;
+  z-index: 999;
+
+  &:hover,
+  &:focus {
+    opacity: 1;
+  }
+`;
+
 interface Params {
   slide: RefObject<HTMLDivElement>;
   children: JSX.Element[];
 }
+
 function Carousel({ slide, children }: Params) {
   const slidesContainer = useRef<HTMLDivElement>(null);
-  const handlePrev = () => {
-    if (slide.current) {
-      const slideWidth = slide.current.clientWidth;
-      if (slidesContainer.current) {
-        slidesContainer.current.scrollLeft -= slideWidth;
-      }
+  const metSlide = (operation: string) => {
+    const slideWidth = slide.current?.clientWidth;
+    if (operation == "-") {
+      slidesContainer.current!.scrollLeft -= slideWidth!;
+      return;
     }
+    slidesContainer.current!.scrollLeft += slideWidth!;
   };
 
-  const handleNext = () => {
-    if (slide.current) {
-      const slideWidth = slide.current.clientWidth;
-      if (slidesContainer.current) {
-        slidesContainer.current.scrollLeft += slideWidth;
-      }
-    }
-  };
+  const handlePrev = () => metSlide("-");
+  const handleNext = () => metSlide("+");
+
+  const JSXBtn = [
+    {
+      click: handlePrev,
+      id: "prev",
+      txt: "<",
+    },
+    {
+      click: handleNext,
+      id: "next",
+      txt: ">",
+    },
+  ];
 
   return (
     <Container>
-      <div className="slider-wrapper">
-        <button
-          onClick={handlePrev}
-          className="slide-arrow"
-          id="slide-arrow-prev"
-        >
-          &#8249;
-        </button>
-        <button
-          onClick={handleNext}
-          className="slide-arrow"
-          id="slide-arrow-next"
-        >
-          &#8250;
-        </button>
-        <div
-          className="slides-container"
-          ref={slidesContainer}
-          id="slides-container"
-        >
-          {children}
-        </div>
-      </div>
+      {JSXBtn.map((v) => (
+        <Btn onClick={v.click} id={v.id} key={v.id}>
+          {v.txt}
+        </Btn>
+      ))}
+      <ContainerSlide ref={slidesContainer}>{children}</ContainerSlide>
     </Container>
   );
 }
