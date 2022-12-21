3315621f32a730fcf59aa2a9a8e90601a13a5079
JosseMontano <josemariazambranamontano123@gmail.com>
2022-12-21 16:37:41 -0400

    save data in cache

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,8 @@ import ToastProvider from "./context/toast/toast";
 import LoadProvider from "./context/nameUser";
 import { HomeContextProvider } from "./context/home/homeContext";
 import { LanguageContextProvider } from "./context/languageContext";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ToastProvider>
@@ -18,3 +20,8 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </ToastProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
