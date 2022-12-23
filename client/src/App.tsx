965dd5f5df73ffa311d85323247267c564020172
JosseMontano <josemariazambranamontano123@gmail.com>
2022-12-22 22:54:18 -0400

    refactor route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
 import { useEffect } from "react";
 import { HashRouter } from "react-router-dom";
 import "./App.css";
-import RouteComponent from "./route";
-
+import { Route, Routes } from "react-router-dom";
+import routes from "./routes/routes";
 const App = () => {
-
   const handle = () => {
     const isProduction = import.meta.env.PROD;
-    if (isProduction && 'serviceWorker' in navigator) {
+    if (isProduction && "serviceWorker" in navigator) {
       navigator.serviceWorker.register("sw.js");
     } else {
-/*       alert("Su navegador no cuenta con la opcion offline"); */
+      /*       alert("Su navegador no cuenta con la opcion offline"); */
     }
   };
   useEffect(() => {
@@ -19,7 +18,12 @@ const App = () => {
 
   return (
     <HashRouter>
-      <RouteComponent />
+      <Routes>
+        {routes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+        <Route path="*" element={<p>Page no found</p>} />
+      </Routes>
     </HashRouter>
   );
 };
