c3f2c4b7234cdc1f4214664162cc6aa69295af68
JosseMontano <josemariazambranamontano123@gmail.com>
2022-12-26 21:55:44 -0400

    delete files of 360

--- vr360/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import RouteComponent from "./route"
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouteComponent />
-  </React.StrictMode>
-)
--- vr360/src/services/http.js
+++
@@ -1,3 +0,0 @@
-/* const endpoint = "http://localhost:3000/"; */
-const endpoint = "https://realestate-server-production.up.railway.app/";
-export default endpoint;
