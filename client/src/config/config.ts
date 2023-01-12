e4ecfb1513fe12b729dcfd21bac30e9f96a87367
JosseMontano <josemariazambranamontano123@gmail.com>
2023-01-12 14:23:36 -0400

    add file env to the frontend

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -4,20 +4,12 @@ interface EnvType {
   backendUrlDevWS: string;
   backendUrlProd: string;
   backendUrlProdWS: string;
-  frontendUrlDev: string;
 }
-/* const env:EnvType = {
+const env: EnvType = {
   backendUrlDev: import.meta.env.VITE_BACKEND_URL_DEV,
   backendUrlProd: import.meta.env.VITE_BACKEND_URL_PROD,
-};
- */
-
-const env: EnvType = {
-  backendUrlDev: "http://localhost:3000/",
-  backendUrlDevWS: "ws://localhost:3000/graphql",
-  backendUrlProd: "https://realestate-server-production.up.railway.app/",
-  backendUrlProdWS: "wss://realestate-server-production.up.railway.app/graphql",
-  frontendUrlDev: "http://127.0.0.1:5173/#/",
+  backendUrlDevWS: import.meta.env.VITE_BACKEND_ULR_DEV_WS,
+  backendUrlProdWS: import.meta.env.VITE_BACKEND_URL_PROD_WS,
 };
 
 export default env;
