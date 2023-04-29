47392e395c315b64ec666878cf3706ceaf80c98a
JosseMontano <josemariazambranamontano123@gmail.com>
2023-04-29 15:18:52 -0400

    fe:fix bug when a person go to the page by url

--- client/src/public/visitUser/index.tsx
+++ client/src/public/visitUser/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import Header from "./components/headers";
 import { marginGlobal, ColorText } from "../../global/styles/globals";
@@ -7,7 +7,7 @@ import Publication from "@/global/components/dynamic/profileVisitUser/publicatio
 import { RealEstate } from "@/global/interfaces/realEstate";
 import { Suspense, useEffect } from "react";
 import { useProfile } from "@/global/context/profileContext";
-
+import {Token} from "@/global/utilities/getTokenCookie"
 const Container = styled.div<{ marginGlobal: string; ColorText: string }>`
   height: 100%;
   margin: ${(props) => props.marginGlobal};
@@ -16,10 +16,19 @@ const Container = styled.div<{ marginGlobal: string; ColorText: string }>`
 
 const VisitUser = () => {
   const { id: idUser, email, realEstate } = useParams();
+
+  let navigate = useNavigate();
+
+  if (!Token) {
+    navigate("/");
+    location.reload();
+  }
+
+
   const idUserNumber = Number(idUser);
   const realEstateNumber = parseFloat(realEstate!);
   const { data, loading, getRealEstate } = useProfile();
-
+ 
   //get cellphone of user
   let dataObj = {} as RealEstate;
   if (data) {
