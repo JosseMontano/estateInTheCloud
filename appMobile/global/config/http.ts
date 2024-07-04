da2e57a5982a82047a230933b3410895f02f0177
Jose Zambrana <eljosema505@gmail.com>
2024-07-04 19:42:08 -0400

    feat: login with google in the front

--- appMobile/global/config/http.ts
+++ appMobile/global/config/http.ts
@@ -1,14 +1,27 @@
+
+//@ts-ignore
+import { EXPO_PUBLIC_CLIENT_ID_GOOGLE, EXPO_PUBLIC_BACK_GOOGLE, EXPO_PUBLIC_API_GOOGLE, EXPO_PUBLIC_OUATH_GOOGLE } from '@env'
 interface ConfigType {
   backendUrl: string;
   backendExpressUrl: string;
   backendExpressUrlWs: string;
   backendPython: string;
+
+  clientIdGoogle: string;
+  backGoogle: string;
+  apiGoogle: string;
+  oauthGoogle: string;
+
 }
 const config: ConfigType = {
   backendUrl: "http://10.0.2.2:9000/api/",
   backendExpressUrl: "http://10.0.2.2:3000/",
   backendExpressUrlWs: "ws://10.0.2.2:3000/graphql",
   backendPython: "http://10.0.2.2:5000/api/",
+  clientIdGoogle: EXPO_PUBLIC_CLIENT_ID_GOOGLE,
+  backGoogle: EXPO_PUBLIC_BACK_GOOGLE,
+  apiGoogle: EXPO_PUBLIC_API_GOOGLE,
+  oauthGoogle: EXPO_PUBLIC_OUATH_GOOGLE
 };
 
 
--- appMobile/screens/login/login.tsx
+++ appMobile/screens/login/login.tsx
@@ -1,6 +1,6 @@
 import { useLinkTo } from "@react-navigation/native";
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import Header from "./components/header";
 import Form from "./components/form";
 import { useHandleSubmit } from "./hooks/useHandleSubmit";
@@ -12,8 +12,12 @@ import {
 } from "../../global/constants/colors/color";
 import { inputStyle } from "../../global/constants/colors/input";
 import { RecuperateAccount } from "./components/recuperateAccount";
+import * as Linking from "expo-linking";
+import * as WebBrowser from "expo-web-browser";
+import config from "../../global/config/http";
 
 export const AuthScreen = () => {
+  const appUrl = Linking.createURL("");
   const { handleSubmit, msgPost, loading } = useHandleSubmit();
 
   const navigate = useLinkTo();
@@ -21,6 +25,50 @@ export const AuthScreen = () => {
   const createAccount = () => {
     navigate("/Register");
   };
+
+  const getParamsStr = (params: Record<string, string | undefined>) => {
+    const arr = [];
+    for (const key in params) {
+      if (params[key]) {
+        arr.push(`${key}=${params[key]}`);
+      }
+    }
+    return "?" + arr.join("&");
+  };
+
+  const getUserData = async (result: any) => {
+    const { url } = result;
+    if (url) {
+      const params = Linking.parse(url) as any;
+      const { userId, name } = params.queryParams;
+      console.log(userId, name);
+    }
+  };
+
+  const handlePress = async () => {
+    const baseUrl = config.oauthGoogle;
+    const params = {
+      response_type: "code",
+      client_id: config.clientIdGoogle,
+      redirect_uri: config.backGoogle + "google",
+      scope: config.apiGoogle,
+      access_type: "offline",
+      prompt: "consent",
+      state: appUrl,
+    };
+    const url = baseUrl + getParamsStr(params);
+    const result = await WebBrowser.openAuthSessionAsync(
+      url,
+      config.backGoogle + "google"
+    );
+
+    getUserData(result);
+  };
+
+  useEffect(() => {
+    Linking.addEventListener("url", getUserData);
+  }, []);
+
   return (
     <View style={stylesLogin.container}>
       <Header />
@@ -36,6 +84,9 @@ export const AuthScreen = () => {
         handleSubmit={() => createAccount()}
         txt="Crear cuenta"
       />
+      <TouchableOpacity onPress={handlePress}>
+        {<Text>Google</Text>}
+      </TouchableOpacity>
     </View>
   );
 };
